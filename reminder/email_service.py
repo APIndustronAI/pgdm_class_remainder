326007bc6533c2ca81b33e0cffa31b40850ab4ec
Arun <arunprakashr123@gmail.com>
2026-03-18 22:06:57 +0530

    Update email_service.py

--- reminder/email_service.py
+++ reminder/email_service.py
@@ -1,29 +1,23 @@
 import smtplib
 import os
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from dotenv import load_dotenv
-
-load_dotenv()
 
 EMAIL = os.getenv("OUTLOOK_EMAIL")
 PASSWORD = os.getenv("OUTLOOK_PASSWORD")
 
-def send_mail(students, subject, link, minutes):
+STUDENTS = ["arunprakashr123@gmail.com"]
 
-    msg = MIMEMultipart()
+def send_mail(subject, minutes, link):
 
-    msg["From"] = str(EMAIL)
-    msg["To"] = ", ".join(students)
-    msg["Subject"] = f"PGDM Cohort3 Reminder: {subject} Weekend Class"
+    server = smtplib.SMTP("smtp.office365.com", 587)
+    server.starttls()
+    server.login(EMAIL, PASSWORD)
 
-    body = f"""
+    message = f"""Subject: PGDM Cohort3 {subject} Weekend Class Reminder
 Reminder 🚨
 
 Hi Students,
 
 This is a reminder that our **{subject}** class starts in **{minutes}** minutes.
-
 Join Link: {link}
 
 Be ready before start time. Join early to avoid attendance issues.
@@ -32,12 +26,5 @@ Thanks,
 AP
 """
 
-    msg.attach(MIMEText(body, "plain", "utf-8"))
-
-    server = smtplib.SMTP("smtp.office365.com", 587)
-    server.starttls()
-    server.login(str(EMAIL), str(PASSWORD))
-
-    server.sendmail(str(EMAIL), students, msg.as_string())
-
+    server.sendmail(EMAIL, STUDENTS, message)
     server.quit()
