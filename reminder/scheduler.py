d06bbe06706db8e90e70438b91287de16102e427
Arun <arunprakashr123@gmail.com>
2026-03-18 19:36:56 +0530

    Update scheduler.py

--- reminder/scheduler.py
+++ reminder/scheduler.py
@@ -6,31 +6,40 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-WINDOW45 = int(os.getenv("REMINDER_WINDOW_45", 12))
-WINDOW10 = int(os.getenv("REMINDER_WINDOW_10", 11))
+WINDOW30 = int(os.getenv("REMINDER_WINDOW_30", 30))
+WINDOW10 = int(os.getenv("REMINDER_WINDOW_10", 10))
 
 STUDENTS = [
     "arunprakashr123@gmail.com"
 ]
 
 def run_scheduler():
-    URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4Ku3ZAYr79s_-mMWSwCdCmWchh3VD0CEXrsVqoz2rVyT95GI-mJeQIRRRvj88Z_u87qJUXHr4dmp6/pub?output=csv"
-    df = pd.read_csv(URL)
+   
 
-    now = datetime.utcnow() + timedelta(hours=5,minutes=30)
+def run_scheduler():
+    SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4Ku3ZAYr79s_-mMWSwCdCmWchh3VD0CEXrsVqoz2rVyT95GI-mJeQIRRRvj88Z_u87qJUXHr4dmp6/pub?output=csv"
+    df = pd.read_csv(SHEET_URL)
 
+    now = datetime.utcnow() + timedelta(hours=5, minutes=30)
     for i in range(len(df)):
 
-        class_time = datetime.strptime(str(df.loc[i,"datetime"]), "%Y-%m-%d %H:%M")
+        subject = df.loc[i, "subject"]
+        link = df.loc[i, "class_link"]
 
-        if df.loc[i,"reminder45_sent"] == "no":
-            if now >= class_time - timedelta(minutes=WINDOW45) and now < class_time - timedelta(minutes=WINDOW45-5):
-                send_mail(STUDENTS, df.loc[i,"subject"], df.loc[i,"link"], WINDOW45)
-                df.loc[i,"reminder45_sent"] = "yes"
+        class_time = datetime.strptime(
+            df.loc[i, "datetime"],
+            "%Y-%m-%d %H:%M"
+        )
 
-        if df.loc[i,"reminder10_sent"] == "no":
-            if now >= class_time - timedelta(minutes=WINDOW10) and now < class_time:
-                send_mail(STUDENTS, df.loc[i,"subject"],df.loc[i,"link"], WINDOW10)
-                df.loc[i,"reminder10_sent"] = "yes"
+        key30 = subject + "_30"
+        key10 = subject + "_10"
 
-    df.to_csv(URL,index=False)
+        if now >= class_time - timedelta(minutes=30):
+            if not already_sent(key30):
+                send_mail(subject, 30, link)
+                mark_sent(key30)
+
+        if now >= class_time - timedelta(minutes=10):
+            if not already_sent(key10):
+                send_mail(subject, 10, link)
+                mark_sent(key10)
