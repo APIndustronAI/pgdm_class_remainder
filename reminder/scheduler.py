40c2e8f9a65864c1d36a3ed3c74141ca85108de9
Arun <arunprakashr123@gmail.com>
2026-03-18 20:05:37 +0530

    Update scheduler.py

--- reminder/scheduler.py
+++ reminder/scheduler.py
@@ -1,27 +1,28 @@
 import pandas as pd
-from datetime import datetime, timedelta
-from email_service import send_mail
 import os
-from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from email_service import send_mail   # ⭐ IMPORTANT IMPORT
 
-load_dotenv()
+SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4Ku3ZAYr79s_-mMWSwCdCmWchh3VD0CEXrsVqoz2rVyT95GI-mJeQIRRRvj88Z_u87qJUXHr4dmp6/pub?output=csv"
+LOG_FILE = "sent_log.txt"
 
-WINDOW30 = int(os.getenv("REMINDER_WINDOW_30", 30))
-WINDOW10 = int(os.getenv("REMINDER_WINDOW_10", 10))
+def already_sent(key):
+    if not os.path.exists(LOG_FILE):
+        return False
+    with open(LOG_FILE, "r") as f:
+        return key in f.read()
 
-STUDENTS = [
-    "arunprakashr123@gmail.com"
-] 
+def mark_sent(key):
+    with open(LOG_FILE, "a") as f:
+        f.write(key + "\n")
+
 def run_scheduler():
-    SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4Ku3ZAYr79s_-mMWSwCdCmWchh3VD0CEXrsVqoz2rVyT95GI-mJeQIRRRvj88Z_u87qJUXHr4dmp6/pub?output=csv"
-    df = pd.read_csv(SHEET_URL)
 
+    df = pd.read_csv(SHEET_URL)
     now = datetime.utcnow() + timedelta(hours=5, minutes=30)
     for i in range(len(df)):
-
         subject = df.loc[i, "subject"]
         link = df.loc[i, "link"]
-
         class_time = datetime.strptime(
             df.loc[i, "datetime"],
             "%Y-%m-%d %H:%M"
@@ -33,7 +34,7 @@ def run_scheduler():
         if now >= class_time - timedelta(minutes=30):
             if not already_sent(key30):
                 send_mail(subject, 30, link)
-                mark_sent(key30)
+                mark_sent(key45)
 
         if now >= class_time - timedelta(minutes=10):
             if not already_sent(key10):
